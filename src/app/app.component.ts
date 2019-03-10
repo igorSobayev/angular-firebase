a25dd3d35b179b0f9dbc0b7568e34bf19ac01730
igorSobayev <isobayev@gmail.com>
2019-03-10 11:13:03 +0100

    Primer commit, empezando a trabajar con firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// Firebase
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebaseTareas';
+  
+  items: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('items').valueChanges();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+// copiado de https://github.com/angular/angularfire2/blob/master/docs/install-and-setup.md
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyADsEMQCkyBX5MqPtVjhhClX7S_se8iPb4",
+    authDomain: "lista-de-tareas-2f081.firebaseapp.com",
+    databaseURL: "https://lista-de-tareas-2f081.firebaseio.com",
+    projectId: "lista-de-tareas-2f081",
+    storageBucket: "",
+    messagingSenderId: "1067363676220"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
